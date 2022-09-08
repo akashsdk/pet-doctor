ad2a72741a68118343c960e17a5af5f017f127fb
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-09-09 00:53:50 +0600

    Update Heder.js

--- src/Component/Heder.js
+++ src/Component/Heder.js
@@ -6,6 +6,7 @@ export default function Heder() {
   return (
     <div className='HeaderBody'>
       <div className='HeaderTite'>
+        
         <Button className='HeaderTiteButton' icon={<LogoutOutlined  className='HeaderTiteIcon'/>} />
         <h1 style={{flexDirection: 'row',display: 'flex',}}>Admin <div className='HeaderTiteHide'>Deshbord</div>
           </h1>
